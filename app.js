944a73e5ee60d3f86353e8f5738a53fcb8d96d07
patelsoham78 <34511151+patelsoham78@users.noreply.github.com>
2022-02-27 08:10:48 +0530

    created server

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.get('/',(req,res) => {
     res.send('Hello World');
 })
 
+const port = 3000;
+const server = app.listen(port, () => {
+    console.log("Server is running.......");
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,2 @@
 const app = require('./app');
 
-const port = 3000;
-const server = app.listen(port, () => {
-    console.log("Server is running.......");
-});
